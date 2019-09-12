120bba2a1d7fe3a07f9013bcf53ea0f7720202f1
Alan Fernando Rincón Vieyra <alan.vieyra376@gmail.com>
2019-09-12 02:54:53 -0500

    Tarea 2: Completada

--- 02_matriz-funciones/02_matriz-funciones.c
+++ 02_matriz-funciones/02_matriz-funciones.c
@@ -2,30 +2,40 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef double (*func)(double);
+#define INFINITE 10000
+#define A 0.0
+#define B 1.0
 
+typedef double (*func)(double);
 func functions[] = {sin, cos};
 
-double integral(func);
+double definiteIntegral(func, double, double);
 
 int main()
 {
-  printf("##### Arreglo de Funciones #####\n\n");
+  printf("##### Integral de Funciones en un Arreglo #####\n\n");
 
-  int functions_size = sizeof(functions) / sizeof(func); // Array size.
+  int functions_size = sizeof(functions) / sizeof(func);
 
   for (int i = 0; i < functions_size; i++)
   {
-    double _integral = integral(functions[i]);
-    printf("Integral de 0 a 1: %lf\n", _integral);
+    double _integral = definiteIntegral(functions[i], A, B);
+    printf("Integral de la funcion %d, de %.1lf a %.1lf: %lf\n", (i+1), A, B, _integral);
   }
 
   return 0;
 }
 
-double integral(func function)
+double definiteIntegral(func f, double a, double b)
 {
-  double result = function(1) - function(0);
+  double _integral = 0.0;
+  int n = INFINITE; // 'n' tiende a infinito.
+  double dx = (b - a) / n;
 
-  return result;
+  for (int i = 1; i<= n; i++) {
+    double xi = a + (dx * i);
+    _integral += f(xi) * dx;
+  }
+
+  return _integral;
 }
