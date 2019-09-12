e4f491aafb4a7ff19e132f59d8733b7f6fd80a5f
Alan Fernando Rincón Vieyra <alan.vieyra376@gmail.com>
2019-09-12 01:56:12 -0500

    Tarea 2: Se implemento arreglo de funciones

--- 02_matriz-funciones/02_matriz-funciones.c
+++ 02_matriz-funciones/02_matriz-funciones.c
@@ -1,10 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+typedef double (*func)(double);
 
+func functions[] = {sin, cos};
 
-int main() {
-  printf("Programa de array de Matrices\n");
-  
+double integral(func);
+
+int main()
+{
+  printf("##### Arreglo de Funciones #####\n\n");
+
+  int functions_size = sizeof(functions) / sizeof(func); // Array size.
+
+  for (int i = 0; i < functions_size; i++)
+  {
+    double _integral = integral(functions[i]);
+    printf("Integral de 0 a 1: %lf\n", _integral);
+  }
+
   return 0;
+}
+
+double integral(func function)
+{
+  double result = function(1) - function(0);
+
+  return result;
 }
