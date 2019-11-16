eec4b0e4da6c5dae93de56cac23943eb918219ec
Alan Fernando Rincon Vieyra <alan.vieyra376@gmail.com>
2019-11-15 22:02:46 -0600

    Tarea 7: Tabla Hash

--- 07_hash/hashTable.cpp
+++ 07_hash/hashTable.cpp
@@ -1,94 +1,109 @@
 #include <iostream>
-#include <string> 
+#include <string>
 
-#define N 7  
+using namespace std;
 
-using namespace std; 
+typedef struct _reg
+{
+   int key;
+   string data;
+   struct _reg *next;
+} reg;
 
-typedef struct _reg {
-    int key; 
-    string data; 
-    struct _reg *next; 
-} reg; 
-
-class hashTable {
-   reg *table[N];
+class hashTable
+{
+   int N;
+   reg **table;
 
 public:
-   hashTable() {
-       int i; 
-       for (i=0; i<N;i++)
-          table[i] = NULL; 
+   hashTable(int n)
+   {
+      N = n;
+      table = (reg**) malloc(sizeof(reg*) * N);
    }
 
-   ~hashTable() {
-       clear(); 
-    }
-
-   int hash(int k) {
-      return k%N; 
+   ~hashTable()
+   {
+      clear();
    }
 
-   int insert(int key, string data) {
-      reg *tmp, *last; 
-      int i = hash(key); 
-      last=table[i]; 
-      tmp = (reg *)malloc(sizeof(reg));  
-      tmp->key = key; 
-      tmp->data = data; 
-      tmp->next = NULL; 
+   int hash(int k)
+   {
+      return k % N;
+   }
 
-      if (last==NULL) {
-         table[i] = tmp; 
-      }  else {
-         while (last->next!=NULL) last = last->next; 
-         last->next = tmp; 
+   int insert(int key, string data)
+   {
+      reg *tmp, *last;
+      int i = hash(key);
+      last = table[i];
+      tmp = (reg *)malloc(sizeof(reg));
+      tmp->key = key;
+      tmp->data = data;
+      tmp->next = NULL;
+
+      if (last == NULL)
+      {
+         table[i] = tmp;
       }
-      return i; 
+      else
+      {
+         while (last->next != NULL)
+            last = last->next;
+         last->next = tmp;
+      }
+      return i;
    }
 
-   void print() {
-      int i; 
-      reg *tmp; 
-      for (i=0; i<N; i++) {
+   void print()
+   {
+      int i;
+      reg *tmp;
+      for (i = 0; i < N; i++)
+      {
          cout << i << ": ";
-         tmp = table[i]; 
-         while(tmp!=NULL) {
-              cout << tmp->key << "," << tmp->data << "|\t";  
-              tmp = tmp->next; 
+         tmp = table[i];
+         while (tmp != NULL)
+         {
+            cout << tmp->key << "," << tmp->data << "|\t";
+            tmp = tmp->next;
          }
-         cout << endl; 
+         cout << endl;
       }
    }
 
-   void clear() {
-     int i; 
-     reg *tmp, *next; 
-     for (i=0; i<N; i++) {
-       next=table[i]; 
-       while(next!=NULL) {
-           tmp=next->next; 
-           free(next); 
-           next=tmp; 
-       }   
-       table[i]=NULL; 
-     }   
+   void clear()
+   {
+      int i;
+      reg *tmp, *next;
+      for (i = 0; i < N; i++)
+      {
+         next = table[i];
+         while (next != NULL)
+         {
+            tmp = next->next;
+            free(next);
+            next = tmp;
+         }
+         table[i] = NULL;
+      }
+      free(table);
    }
 };
 
-int main() {
-    hashTable t; 
-    
-    //t.print(); 
+int main()
+{
+   hashTable t(10);
 
-    t.insert(133, "Juanito"); 
-    t.insert(4, "Pedro"); 
-    t.insert(19, "Hugo"); 
-    t.insert(33, "Paco"); 
-    t.insert(44, "Luis");
-    t.insert(100, "Tio Mc"); 
- 
-    t.print();
-    t.clear();    
+   //t.print();
+
+   t.insert(133, "Juanito");
+   t.insert(4, "Pedro");
+   t.insert(19, "Hugo");
+   t.insert(33, "Paco");
+   t.insert(44, "Luis");
+   t.insert(100, "Tio Mc");
+
+   t.print();
+   t.clear();
 }
-
